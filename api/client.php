733cdb2633487b31802e39a7ae8a79c2bea16608
mcabot <mcabot@iesemilidarder.com>
2018-01-26 04:00:51 -0500

    Eliminam validacio per fitxer credentials

--- api/client.php
+++ api/client.php
@@ -2,7 +2,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config.php';
 
-
 /**
  * Returns an authorized API client.
  * @return Google_Client the authorized client object
--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php
 define('APPLICATION_NAME', 'GestIB 2 Google');
-define('CREDENTIALS_PATH', '~/.credentials/admin-directory_v1-gestib2google.json');
 define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
 
 define('DOMAIN', 'iesemilidarder.com');
@@ -14,17 +13,3 @@ define('SCOPES', implode(' ', array(
   Google_Service_Directory::ADMIN_DIRECTORY_ORGUNIT
   )
 ));
-
-/**
- * Expands the home directory alias '~' to the full path.
- * @param string $path the path to expand.
- * @return string the expanded path.
- */
-function expandCredentialsDirectory($path) {
-  /*$homeDirectory = getenv('HOME');
-  if (empty($homeDirectory)) {
-    $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
-  }
-  return str_replace('~', realpath($homeDirectory), $path);*/
-  return str_replace('~', realpath(__DIR__), $path);
-}
