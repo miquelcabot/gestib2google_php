8112f72b8775efcf08bb385dd9091fb550adb2dc
mcabot <mcabot@iesemilidarder.com>
2018-01-30 13:38:46 -0500

    Canviam strtolower a mb_strtolower

--- api/xmlfile.php
+++ api/xmlfile.php
@@ -93,10 +93,10 @@ function readXmlUsers($xmlfile, $xmlgroups, $xmltutors, $xmltimetable) {
          
         $xmlusers[strval($student['codi'])] = new DomainUser(
             strval($student['codi']),
-            ucwords(mb_strtolower($student['nom']),'UTF-8'), 
-            ucwords(mb_strtolower($student['ap1']." ".$student['ap2']),'UTF-8'),
-            ucwords(mb_strtolower($student['ap1']),'UTF-8'), 
-            ucwords(mb_strtolower($student['ap2']),'UTF-8'), 
+            ucwords(mb_strtolower($student['nom'],'UTF-8')), 
+            ucwords(mb_strtolower($student['ap1']." ".$student['ap2'],'UTF-8')),
+            ucwords(mb_strtolower($student['ap1'],'UTF-8')), 
+            ucwords(mb_strtolower($student['ap2'],'UTF-8')), 
             NULL,            // domainemail
             FALSE,           // suspended
             FALSE,           // teacher 
@@ -115,10 +115,10 @@ function readXmlUsers($xmlfile, $xmlgroups, $xmltutors, $xmltimetable) {
         
         $xmlusers[strval($teacher['codi'])] = new DomainUser(
             strval($teacher['codi']),
-            ucwords(mb_strtolower($teacher['nom']),'UTF-8'), 
-            ucwords(mb_strtolower($teacher['ap1']." ".$teacher['ap2']),'UTF-8'),
-            ucwords(mb_strtolower($teacher['ap1']),'UTF-8'), 
-            ucwords(mb_strtolower($teacher['ap2']),'UTF-8'), 
+            ucwords(mb_strtolower($teacher['nom'],'UTF-8')), 
+            ucwords(mb_strtolower($teacher['ap1']." ".$teacher['ap2'],'UTF-8')),
+            ucwords(mb_strtolower($teacher['ap1'],'UTF-8')), 
+            ucwords(mb_strtolower($teacher['ap2'],'UTF-8')), 
             NULL,            // domainemail
             FALSE,           // suspended
             TRUE,            // teacher 
